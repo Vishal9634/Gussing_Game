fbf288de4b55ebfecf30925d6f42f9bbb1124965
vishal singh <vishalofficial9634@gmail.com>
2022-10-12 16:54:11 +0530

    initial commit

---
+++ GameLauncher.java
@@ -0,0 +1,10 @@
+public class GameLauncher {
+    public static void main(String [] args)
+    {
+        //this is a has a relationship
+        GuessGame game=new GuessGame();
+        //object creation
+        game.StartGame();
+        //method calling
+    }
+}
